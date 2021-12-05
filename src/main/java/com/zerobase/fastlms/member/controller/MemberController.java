629c69520764af20380959ea5c139d3cfd456bf7
jk960903 <jk960903@naver.com>
2021-12-05 23:31:22 +0900

    add Service

--- src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
+++ src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
@@ -1,24 +1,21 @@
 package com.zerobase.fastlms.member.controller;
 
-import com.zerobase.fastlms.member.Repostiory.MemberRepository;
+import com.zerobase.fastlms.member.Service.MemberService;
 import com.zerobase.fastlms.member.entity.Member;
 import com.zerobase.fastlms.member.model.MemberInput;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.time.LocalDateTime;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 
-    private final MemberRepository memberRepository;
-
-    public MemberController(MemberRepository memberRepository){
-        this.memberRepository = memberRepository;
-    }
+    private final MemberService memberService;
 
     @GetMapping("/member/register")
     public String register(){
@@ -30,13 +27,7 @@ public class MemberController {
     public String registerSubmit(HttpServletRequest request, HttpServletResponse response, MemberInput parameter){
         Member member = new Member();
         try{
-            member.setUserId(parameter.getUserId());
-            member.setPassword(parameter.getPassword());
-            member.setPhone(parameter.getPhone());
-            member.setUserName(parameter.getUserName());
-            member.setRegdate(LocalDateTime.now());
-
-            memberRepository.save(member);
+            memberService.register(parameter);
         }catch(Exception e){
 
         }
