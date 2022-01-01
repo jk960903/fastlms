da6ff5373626c6cdd2fdf7147cdef582403528d3
jk960903 <jk960903@naver.com>
2022-01-01 23:51:16 +0900

    리스트 수정 및 mabatis 설정

--- src/main/java/com/zerobase/fastlms/admin/member/controller/AdminMemberController.java
+++ src/main/java/com/zerobase/fastlms/admin/member/controller/AdminMemberController.java
@@ -4,6 +4,7 @@ import com.zerobase.fastlms.member.Service.MemberService;
 import com.zerobase.fastlms.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
