4bc6c084fb5d92ce3260b2ace91fbc80da2af8db
jk960903 <jk960903@naver.com>
2022-01-10 17:28:36 +0900

    6주차 과제 commit email service and interface

--- src/main/java/com/zerobase/fastlms/member/Repostiory/EmailRepository.java
+++ src/main/java/com/zerobase/fastlms/member/Repostiory/EmailRepository.java
@@ -3,5 +3,8 @@ package com.zerobase.fastlms.member.Repostiory;
 import com.zerobase.fastlms.member.entity.EmailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmailRepository extends JpaRepository<EmailEntity,String> {
+    Optional<EmailEntity> findById(int id);
 }
